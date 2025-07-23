9a35a30d8034b1b35d40a7abadb841018f0cb083
hlee.grapicar <119303558+notmuchhayden@users.noreply.github.com>
2025-07-23 12:43:22 +0900

    skip-gram 단어사전 만들기 추가

--- skipgram/VanillaSkipgram.py
+++ skipgram/VanillaSkipgram.py
@@ -13,3 +13,33 @@ class VanillaSkipgram(nn.Module):
         output = self.linear(embeddings)
         return output
     
+    
+import pandas as pd
+from Korpora import Korpora
+from konlpy.tag import Okt
+
+corpus = Korpora.load('nsmc')
+corpus = pd.DataFrame(corpus.test)
+
+tokenizer = Okt()
+tokens = [tokenizer.morphs(review) for review in corpus.text]
+print(tokens[:3]) 
+
+
+
+from collections import Counter
+def build_vocab(corpus, n_vocab, special_tokens):
+    counter = Counter()
+    for tokens in corpus:
+        counter.update(tokens)
+    vocab = special_tokens
+    for token, count in counter.most_common(n_vocab):
+        vocab.append(token)
+    return vocab
+
+vocab = build_vocab(corpus=tokens, n_vocab=5000, special_tokens=["<unk>"])
+token_to_id = {token: idx for idx, token in enumerate(vocab)}
+id_to_token = {idx: token for idx, token in enumerate(vocab)}
+
+print(vocab[:10])
+print(len(vocab))
